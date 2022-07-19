82f0d54dea58cdd34a60eed771faa35cb84b9465
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-07-19 18:05:54 +0100

    feat: add CTA.jsx with downloadable CV and HeaderSocials.jsx with links to social media profiles

--- anastasia-starostina-portfolio/src/components/header/Header.jsx
+++ anastasia-starostina-portfolio/src/components/header/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+// import ME from "../../assets/ME.jfif"
+//Commented out the image while searching for a more suitable one
 
- const Header = () => {
+const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+  <header>
+    <div className=".container.header_container">
+    <h5>Hey, I'm</h5>
+    <h1>Anastasia</h1>
+    <h5 className="text-light">Fullstack Developer specializing in UI and UX Design.</h5>
+    <CTA/>
 
-export default Header
+    <div className="me">
+      {/* <img src={ME} alt="me" /> */}
+    </div>
+
+    <a href="#contact" className="scroll_down">Scroll Down</a>
+    </div>
+
+  </header>
+  )
+};
+
+export default Header;
---
+++ anastasia-starostina-portfolio/src/components/header/HeaderSocails.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderSocails = () => {
+  return (
+    <div className="header_socials">
+      <a
+        href="https://www.linkedin.com/in/anastasia-starostina-900bb2130"
+        targer="_blank"></a>
+      <a href="https://github.com/anastasia-starostina" targer="_blank"></a>
+      <a href="https://twitter.com/ShternAnastasia" targer="_blank"></a>
+    </div>
+  );
+}
+
+export default HeaderSocails
