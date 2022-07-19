159b074b68ad5e61f706791fb110bbbc524737fd
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-07-19 19:46:03 +0100

    feat: add react icons in Nav.jsx

--- anastasia-starostina-portfolio/src/components/nav/Nav.jsx
+++ anastasia-starostina-portfolio/src/components/nav/Nav.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import { RiProjectorFill } from "react-icons/ri";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#portfolio">
+        <RiProjectorFill />
+      </a>
+      <a href="#services">
+        <AiOutlineHome />
+      </a>
+      <a href="#contact">
+        <AiOutlineHome />
+      </a>
+    </nav>
+  );
 }
 
 export default Nav
