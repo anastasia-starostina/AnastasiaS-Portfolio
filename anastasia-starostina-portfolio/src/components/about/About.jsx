c290fc8b030cb0f9b79b3683024bb435b79400ee
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-07-21 08:48:42 +0100

    feat: add react icons and cards in about me section in About.jsx

--- anastasia-starostina-portfolio/src/components/about/About.jsx
+++ anastasia-starostina-portfolio/src/components/about/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import ME from "../../assets/ME.jfif"
+import {FiAward} from 'react-icons/fi'
+import {FiUsers} from "react-icons/fi"
+import {VscFolderLibrary} from "react-icons/vsc"
 
 const About = () => {
   return (
-    <section id="about">About</section>
-  )
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About me</h2>
+      <div className="container about_container">
+        <div className="about__me">
+          <div className="about__me image">
+            <img src={ME} alt="About_Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FiAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>6 Months</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>300+ Worlwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud.
+          </p>
+          <a href="#contact" className ="btn btn-primary">Let's Talk</a>
+
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About
