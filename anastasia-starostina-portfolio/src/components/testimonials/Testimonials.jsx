0efab606039ff9fefbb621c80364f9df08529e6a
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-08-02 18:52:37 +0100

    fix: remove testimonials and services from the rendered page

--- anastasia-starostina-portfolio/src/components/testimonials/Testimonials.jsx
+++ anastasia-starostina-portfolio/src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './testimonials.css'
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials"></section>
   )
 }
 
